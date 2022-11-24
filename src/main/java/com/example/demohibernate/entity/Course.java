fd4363216e476e923d7827656e87196ba8482f6d
Farhad <fgadirli@turanbank.az>
2022-11-24 16:43:55 +0400

    create Entity Course

---
+++ src/main/java/com/example/demohibernate/entity/Course.java
@@ -0,0 +1,29 @@
+package com.example.demohibernate.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    protected Course() {
+    }
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
