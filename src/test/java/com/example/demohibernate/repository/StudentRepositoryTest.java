2934c28ce3f3d006bf1908f7e688d83bac07f2b4
Farhad <fgadirli@turanbank.az>
2022-11-25 16:57:43 +0400

    Retrieving Student with Passport and Eager Fetch

---
+++ src/test/java/com/example/demohibernate/repository/StudentRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.demohibernate.repository;
+
+import com.example.demohibernate.DemoHibernateApplication;
+import com.example.demohibernate.entity.Course;
+import com.example.demohibernate.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = DemoHibernateApplication.class)
+class StudentRepositoryTest {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    StudentRepository repository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void retrieve_student_and_passport_details() {
+        Student student = em.find(Student.class, 2);
+        logger.info("student -> {}", student);
+        logger.info("passport -> {}", student.getPassport());
+    }
+
+}
