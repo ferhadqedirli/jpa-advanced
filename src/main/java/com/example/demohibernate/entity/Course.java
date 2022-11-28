7753ef5883b9c3e7e0f45f8ef173fba8bc120be2
ferhadqedirli <ferhadqedirli38@gmail.com>
2022-11-28 21:35:36 +0400

    ManyToOne Mapping - Implementing the Mapping

--- src/main/java/com/example/demohibernate/entity/Course.java
+++ src/main/java/com/example/demohibernate/entity/Course.java
@@ -5,6 +5,8 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "course")
@@ -25,6 +27,9 @@ public class Course {
 
     @CreationTimestamp
     private LocalDateTime createdDate;
+
+    @OneToMany(mappedBy = "course")
+    private final List<Review> reviews = new ArrayList<>();
 
     protected Course() {
     }
@@ -52,6 +57,18 @@ public class Course {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void addReview(Review review) {
+        this.reviews.add(review);
+    }
+
+    public void removeReview(Review review) {
+        this.reviews.remove(review);
     }
 
     @Override
