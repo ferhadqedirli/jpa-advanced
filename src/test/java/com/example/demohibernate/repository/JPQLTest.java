6ebc4609fc44e059b2f1112619fceabb07340690
Farhad <fgadirli@turanbank.az>
2022-12-12 11:23:41 +0400

    JPQL - Using Joins

--- src/test/java/com/example/demohibernate/repository/JPQLTest.java
+++ src/test/java/com/example/demohibernate/repository/JPQLTest.java
@@ -2,6 +2,7 @@ package com.example.demohibernate.repository;
 
 import com.example.demohibernate.DemoHibernateApplication;
 import com.example.demohibernate.entity.Course;
+import com.example.demohibernate.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -66,4 +67,40 @@ class JPQLTest {
         logger.info("Courses with more students -> {}", resultList);
     }
 
+    @Test
+    void jpql_students_with_passports_in_a_certain_pattern() {
+        TypedQuery<Student> query = em.createQuery("select s from Student s where s.passport.number like '%257%'", Student.class);
+        List<Student> resultList = query.getResultList();
+        logger.info("Results -> {}", resultList);
+    }
+
+    @Test
+    void join() {
+        Query query = em.createQuery("select c, s from Course c join c.students s");
+        List<Object[]> resultList = query.getResultList();
+        logger.info("Results size -> {}", resultList.size());
+        for (Object[] result : resultList) {
+            logger.info("Course {} Student {}", result[0], result[1]);
+        }
+    }
+
+    @Test
+    void left_join() {
+        Query query = em.createQuery("select c, s from Course c left join c.students s");
+        List<Object[]> resultList = query.getResultList();
+        logger.info("Results size -> {}", resultList.size());
+        for (Object[] result : resultList) {
+            logger.info("Course {} Student {}", result[0], result[1]);
+        }
+    }
+
+    @Test
+    void cross_join() {
+        Query query = em.createQuery("select c, s from Course c, Student s");
+        List<Object[]> resultList = query.getResultList();
+        logger.info("Results size -> {}", resultList.size());
+        for (Object[] result : resultList) {
+            logger.info("Course {} Student {}", result[0], result[1]);
+        }
+    }
 }
