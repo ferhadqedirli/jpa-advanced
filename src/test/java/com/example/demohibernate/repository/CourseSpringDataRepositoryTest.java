d066623358f13e7f004224dc568f17a036261971
Farhad <fgadirli@turanbank.az>
2022-12-13 11:41:29 +0400

    Testing the Spring Data JPA Repository with findById

---
+++ src/test/java/com/example/demohibernate/repository/CourseSpringDataRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.example.demohibernate.repository;
+
+import com.example.demohibernate.DemoHibernateApplication;
+import com.example.demohibernate.entity.Course;
+import com.example.demohibernate.entity.Review;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = DemoHibernateApplication.class)
+class CourseSpringDataRepositoryTest {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    CourseSpringDataRepository repository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void findById_coursePresent() {
+        Optional<Course> courseOptional = repository.findById(10001);
+        assertTrue(courseOptional.isPresent());
+    }
+
+    @Test
+    void findById_courseNotPresent() {
+        Optional<Course> courseOptional = repository.findById(20001);
+        assertFalse(courseOptional.isPresent());
+    }
+}
