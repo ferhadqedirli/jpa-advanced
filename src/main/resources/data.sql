d09bb6b973439fc97d44b34041f92056bfafe5da
Farhad <fgadirli@turanbank.az>
2022-12-13 12:48:13 +0400

    Pagination using Spring Data JPA Repository

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,20 @@ insert into course (id, name, created_date, last_updated_date)
 values (10003, 'Spring in 50 steps', current_timestamp, current_timestamp);
 insert into course (id, name, created_date, last_updated_date)
 values (10004, 'Spring Boot in 100 steps', current_timestamp, current_timestamp);
+insert into course (id, name, created_date, last_updated_date)
+values (10005, 'Dummy1', current_timestamp, current_timestamp);
+insert into course (id, name, created_date, last_updated_date)
+values (10006, 'Dummy2', current_timestamp, current_timestamp);
+insert into course (id, name, created_date, last_updated_date)
+values (10007, 'Dummy3', current_timestamp, current_timestamp);
+insert into course (id, name, created_date, last_updated_date)
+values (10008, 'Dummy4', current_timestamp, current_timestamp);
+insert into course (id, name, created_date, last_updated_date)
+values (10009, 'Dummy5', current_timestamp, current_timestamp);
+insert into course (id, name, created_date, last_updated_date)
+values (10010, 'Dummy6', current_timestamp, current_timestamp);
+insert into course (id, name, created_date, last_updated_date)
+values (10011, 'Dummy7', current_timestamp, current_timestamp);
 
 insert into passport (id, number) values (40001, 'AA3257845');
 insert into passport (id, number) values (40002, 'AS6956654');
--- src/test/java/com/example/demohibernate/repository/CourseSpringDataRepositoryTest.java
+++ src/test/java/com/example/demohibernate/repository/CourseSpringDataRepositoryTest.java
@@ -7,6 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 
@@ -49,5 +52,16 @@ class CourseSpringDataRepositoryTest {
         Sort sort = Sort.by("name").descending();
 
         logger.info("Sorted Courses -> {}", repository.findAll(sort));
+    }
+
+    @Test
+    void pagination() {
+        PageRequest pageRequest = PageRequest.of(0, 3);
+        Page<Course>  firstPage = repository.findAll(pageRequest);
+        logger.info("First Page -> {}", firstPage.getContent());
+
+        Pageable secondPageable = firstPage.nextPageable();
+        Page<Course> secondPage = repository.findAll(secondPageable);
+        logger.info("Second Page -> {}", secondPage.getContent());
     }
 }
